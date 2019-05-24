abc50928c931fc36e3f1db25ce274e16eb8b391b
henryLocal <none@example.com>
2019-05-24 16:06:44 +0200

    Base Architecture

--- Azure_Get_Data/data.tf
+++
@@ -1,8 +0,0 @@
-data "azurerm_network_interface" "test" {
-  name                = "azure-vm527"
-  resource_group_name = "Azure_Data"
-}
-
-output "network_interface_id" {
-  value = "${data.azurerm_network_interface.test.location}"
-}
